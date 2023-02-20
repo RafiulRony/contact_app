2e54e85250dacd71a43c9af8f3bba175dae0154c
rafiul-islam <rafiulrony.cse001@gmail.com>
2023-02-20 19:46:29 +0600

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+function getContacts() {
+    return [
+        1 =>['name'=>'Name 1', 'phone'=>'1234567890'],
+        2 =>['name'=>'Name 2', 'phone'=>'2345678901'],
+        3 =>['name'=>'Name 3', 'phone'=>'3456789012'],
+    ];
+}
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/contacts', function(){
+    $companies = [
+        1 => ['name' => 'Company One', 'contacts' => 3],
+        2 => ['name' => 'Company Two', 'contacts' => 5]
+    ];
+    $contacts = getContacts();
+    return view('contacts.index', compact('contacts', 'companies'));
+})->name('contacts.index');
+    
+Route::get('/contacts/create', function(){
+    return view('contacts.create');
+})->name('contacts.create');
+    
+Route::get('/contacts/{id}', function($id){
+    $contacts=getContacts();
+    abort_if(!isset($contacts[$id]), 404);
+    // abort_unless(isset($contacts[$id]), 404);
+
+    $contact=$contacts[$id]; 
+    return view('contacts.show')->with('contact', $contact);
+})->name('contacts.show');
+
+Route::Fallback(function(){
+    return "<h1>Sorry, the page does not exist</h1>";
+});
+// ->whereNumber('id');
+
+
+// Route::get('/companies/{name?}', function($name = null) {
+//     if ($name){
+//         return "Company ". $name;
+//     }
+//     else {
+//         return "All companies";
+//     }
+// })->whereAlphaNumeric('name');
+
+
